f2abb9102ea927ae007079d52ccf333761021be9
niklaskon <niklas.kondler@gmx.at>
2022-10-10 20:58:45 +0200

    Windows-Rechner? NEIN Niklas hat etwas Besseres! DER NIKRECHNER3000 JETZT NUR FÜR 199,99 Anzahlung!

--- Labradoodle/221010-OhmschesGesetz/Program.cs
+++ Labradoodle/221010-OhmschesGesetz/Program.cs
@@ -10,6 +10,24 @@ namespace _221010_OhmschesGesetz
     {
         static void Main(string[] args)
         {
+            double r;
+            Console.WriteLine("Geben Sie den Wert des Widerstandes ein!");
+            r=Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(r+" Ohm");
+
+            double i;
+            Console.WriteLine("Geben Sie den Wert der Stromstärke ein!");
+            i = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(i+" A");
+
+            double u;
+            u = r * i;
+            Console.WriteLine("Die Spannung beträgt "+u+"V");
+            Console.ReadKey();
+
+            
+            
+
         }
     }
 }
